6b4e1491c5f0048fc179b697708a3e771110c887
Jess <jess_ngan@dlsu.edu.ph>
2021-05-31 14:33:45 +0800

    Pushed about us UI changes

--- JJMuseum/app/src/main/java/ph/edu/dlsu/s12/jandj/jjmuseum/AboutUsActivity.java
+++ JJMuseum/app/src/main/java/ph/edu/dlsu/s12/jandj/jjmuseum/AboutUsActivity.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 public class AboutUsActivity extends AppCompatActivity {
 
     private ImageView back_button;
-    private TextView msgDirectorsTv;
+    private TextView msgDirectorsTv, aboutDescTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +30,10 @@ public class AboutUsActivity extends AppCompatActivity {
 
     private void init() {
         back_button = findViewById(R.id.back_button);
+        aboutDescTv = findViewById(R.id.aboutDescTv);
         msgDirectorsTv = findViewById(R.id.msgDirectorsTv);
 
-        msgDirectorsTv.setText("The premier repository and custodian of the country’s heritage introduces its significant collections, and shares to a wider audience on-line its efforts to protect, conserve and promote the natural and cultural heritage of the Filipino people. We hope that through this website, we can stimulate your interest to visit us and appreciate in person the legacies that characterize and make up the Filipino identity.");
+        aboutDescTv.setText("The premier repository and custodian of the country’s heritage introduces its significant collections, and shares to a wider audience on-line its efforts to protect, conserve and promote the natural and cultural heritage of the Filipino people. We hope that through this website, we can stimulate your interest to visit us and appreciate in person the legacies that characterize and make up the Filipino identity.");
+        msgDirectorsTv.setText("The interdisciplinary nature of the work in the JJ Museum also encourages exchange of information and collaborative strategies towards achieving its goals as a cultural, scientific, and educational institution. As we make accessible the museum’s resources in the web, we also invite everyone to engage and commit themselves in the continuous endeavors of heritage preservation not only in the Philippines but throughout the world.");
     }
 }
